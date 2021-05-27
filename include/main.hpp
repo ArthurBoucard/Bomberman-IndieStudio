1cbf686360df6596bcf75d4d4fa2ef1a14d8038d
larroyo3 <lucas.arroyo@epitech.eu>
2021-05-27 16:51:43 +0200

    init push

---
+++ include/main.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-arthur.boucard
+** File description:
+** main
+*/
+
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+
+#endif /* !MAIN_HPP_ */
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-arthur.boucard
+** File description:
+** main
+*/
+
