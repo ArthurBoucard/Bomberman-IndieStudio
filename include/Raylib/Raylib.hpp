8d224c40931250101b6d9e4892027dbb4bb2d8e3
Arthur <arthur.boucard@hotmail.fr>
2021-05-31 15:04:21 +0200

    added encapsulation for window

---
+++ include/Raylib/Raylib.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-arthur.boucard
+** File description:
+** Raylib
+*/
+
+#ifndef RAYLIB_HPP_
+#define RAYLIB_HPP_
+
+#include "raylib.h"
+
+#include "Window.hpp"
+
+namespace Raylib {}
+
+#endif /* !RAYLIB_HPP_ */
