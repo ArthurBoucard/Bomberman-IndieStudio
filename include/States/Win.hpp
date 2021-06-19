512825492d3ff63842cc48fcae31fe27b8aa89d0
Camille Caschili-Lucas <camille.caschili-lucas@epitech.eu>
2021-06-20 00:04:50 +0200

    Add Win screen

--- include/States/Win.hpp
+++ include/States/Win.hpp
@@ -9,20 +9,41 @@
 #define WIN_HPP_
 
 #include "../StatesManagement/State.hpp"
+#include "../States/Menu.hpp"
 
 class Win : public State
 {
-    public:
-        Win();
-        ~Win();
+public:
+    Win(int nbPlayer, int nbIA, int skin1, int skin2);
+    ~Win();
 
-        void Draw();
-        void Update();
-        void Clear();
-        void HandleInput();
-        void Reset();
+    void Draw();
+    void Update();
+    void Clear();
+    void HandleInput();
+    void Reset();
 
-    private:
+    bool CheckMouse(Vector2, Raylib::Rectangle, Raylib::Text, int state);
+
+private:
+    Raylib::Texture2D _bgWin;
+
+    Raylib::Rectangle _rectBack;
+    Raylib::Rectangle _rectAgain;
+
+    int _screenWidth;
+    int _screenHeight;
+
+    Vector2 _mouse;
+
+    Raylib::Text _win;
+    Raylib::Text _back;
+    Raylib::Text _again;
+
+    int _nbPlayer;
+    int _nbIA;
+    int _skin1;
+    int _skin2;
 };
 
 #endif /* !WIN_HPP_ */
