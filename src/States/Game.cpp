2c5ba8a1608fac99542aeab648f3743a9291d9a1
Arthur <arthur.boucard@hotmail.fr>
2021-06-19 17:40:52 +0200

    fix typo

--- src/States/Game.cpp
+++ src/States/Game.cpp
@@ -489,7 +489,7 @@ void Game::Update()
             }
         }
     }
-    // AI movment
+    // AI movement
     for (int i = 0; i < _playerList.size(); i++)
     {
         if (_playerList[i]->getPlayerID() >= 2)
