01d176bd2d7fde6529b2b28d16b0e38074c70236
Sebastien <sebastiensrn@pop-os.localdomain>
2021-06-19 15:11:18 +0200

    cpp for load tuto

---
+++ src/States/HowToPlay.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-LYN-4-1-indiestudio-arthur.boucard
+** File description:
+** HowToPlay
+*/
+
+#include "../../include/States/HowToPlay.hpp"
+
+
+HowToPlay::HowToPlay()
+{
+    _bg.LoadFile("../assets/pictures/howtoplay.png");
+}
+
+HowToPlay::~HowToPlay()
+{
+}
+
+void HowToPlay::Draw()
+{
+}
+
+void HowToPlay::Update()
+{}
+
+void HowToPlay::Clear()
+{}
+
+void HowToPlay::HandleInput()
+{}
+
+void HowToPlay::Reset()
+{}
