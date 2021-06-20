534c4115c26ecad03d0012675fa945616c60236d
Neyross <thibault.bordenave@epitech.eu>
2021-06-20 19:09:11 +0200

    reuse variables because of push before and fix compilation with win

--- src/States/Game.cpp
+++ src/States/Game.cpp
@@ -586,7 +586,7 @@ void Game::Update()
     for (std::size_t i = 0; i < _bombList.size(); i++)
     {
         clock_t end = clock();
-        if (end - _bombList[i]->getClock() >= 900000)
+        if (end - _bombList[i]->getClock() >= time_tbomb)
             _bombList[i]->setIsExplode(true);
     }
     // Make bomb explode
@@ -614,7 +614,7 @@ void Game::Update()
     for (std::size_t i = 0, p = 0, p2 = 0, k = 0, m = 0; i < _flameList.size(); i++)
     {
         clock_t end = clock();
-        if (end - _flameList[i]->getClock() >= 30000)
+        if (end - _flameList[i]->getClock() >= time_bomb)
         {
             _explosionBomb.Play();
             _flameList[i]->resetClock();
@@ -697,7 +697,7 @@ void Game::Update()
     {
         if (_playerList[i]->getPlayerID() >= 2)
         {
-            if (clock() - _playerList[i]->getClock() > 90000)
+            if (clock() - _playerList[i]->getClock() > time_ai)
             {
                 _playerList[i]->setClock(clock());
                 for (int n = 0; n < _positionList.size(); n++)
@@ -717,7 +717,7 @@ void Game::Update()
             for (i = 0; _playerList[i]->getPlayerID() != 0; i++)
                 ;
             if (_playerList[i]->getIsAlive() == true)
-                _context->TransitionTo(new Win(_nbPlayer, _nbIA, _skinChoicePl1, _skinChoicePl2, _skinChoicePl1));
+                _context->TransitionTo(new Win(_nbPlayer, _nbIA, _skinChoicePl1, _skinChoicePl2, _playerList[i]->getPlayerName()));
             else
                 _context->TransitionTo(new GameOver(_nbPlayer, _nbIA, _skinChoicePl1, _skinChoicePl2));
         }
